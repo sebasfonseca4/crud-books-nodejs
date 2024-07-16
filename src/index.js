5810dc5bb753a20a47efe1b9f65355a8dc64236e
Sebastian Fonseca <sebastian.fonseca@soldife.com>
2024-07-16 18:35:22 -0500

    adjst index

--- src/index.js
+++ src/index.js
@@ -19,5 +19,5 @@ app.use('/books', bookRoutes)
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-    console.log(`Server is running on port http://localhost:${port}`)
+    console.log(`Server is running on port ${port}`)
 })
